e1c9ba348f60c9f29ff6935126cf528eedecea79
Duncan Burggraf <dcburggraf@gmail.com>
2016-06-22 13:53:52 -0700

    format and function names

--- include/private_xspng.h
+++ include/private_xspng.h
@@ -6,38 +6,56 @@
 #include <stdio.h> //FILE*
 
 #ifdef __cplusplus
-namespace xspng {
-	extern "C" {
+extern "C" {
 #endif
 
-		typedef struct {
-			xspng_int length;
-			xspng_byte *sig;
-			xspng_byte *buffer; //literally (sig+4)
-			xspng_int crc;
-		} xspng_chunk;
-
-		typedef xspng_chunk* xspng_chunkp;
-
-		void xspng_write_file_sig(FILE* fout);
-		void xspng_write_chunk(FILE* fout, xspng_chunkp chunk);
-
-		void xspng_calc_crc(xspng_chunkp chunk);
-		void xspng_chunk_deflate(xspng_chunkp chunk);
-		void xspng_chunk_allocate(xspng_chunkp chunk);
-		void xspng_free_chunk(xspng_chunkp chunk);
-		void xspng_chunk_set_sig(xspng_chunkp chunk, xspng_byte *sig);
-		void xspng_chunk_put_int(xspng_chunkp chunk, int i, xspng_int v);
-		void xspng_chunk_put_byte(xspng_chunkp chunk, int i, xspng_byte b);
-
-		xspng_chunkp xspng_make_IHDR(xspng_imagep img);
-		xspng_chunkp xspng_make_IDAT(xspng_imagep img);
-		xspng_chunkp xspng_make_IEND();
-
+	typedef struct {
+		xspng_int length;
+		xspng_byte *sig;
+		xspng_byte *buffer; //literally (sig+4)
+		xspng_int crc;
+	} xspng_chunk;
+	
+	typedef xspng_chunk* xspng_chunkp;
+	
+	//return a new chunk object with no allocated buffer
+	xspng_chunkp xspng_chunk_new();
+	
+	//free a chunk objects buffer and then the object itself
+	void xspng_chunk_free(xspng_chunkp p);
+	
+	//free a chunk buffer
+	void xspng_chunk_clear(xspng_chunkp chunk);
+	
+	//allocate (or reallocate) a chunk's buffer
+	void xspng_chunk_allocate(xspng_chunkp chunk);
+	
+	//set a chunk's 4-byte signature
+	void xspng_chunk_set_sig(xspng_chunkp chunk, xspng_byte *sig);
+	
+	//transform chunk-data to zlib format
+	void xspng_chunk_deflate(xspng_chunkp chunk);
+	
+	//calculate and set a chunk's crc value
+	void xspng_chunk_set_crc(xspng_chunkp chunk);
+	
+	//write a 4-byte int into the chunk buffer
+	void xspng_chunk_put_int(xspng_chunkp chunk, int i, xspng_int v);
+	
+	//write a single byte into the chunk buffer
+	void xspng_chunk_put_byte(xspng_chunkp chunk, int i, xspng_byte b);
+	
+	//create chunks from an image buffer
+	xspng_chunkp xspng_make_IHDR(xspng_imagep img);
+	xspng_chunkp xspng_make_IDAT(xspng_imagep img);
+	xspng_chunkp xspng_make_IEND();
+	
+	//write chunks to file
+	void xspng_write_file_sig(FILE* fout);
+	void xspng_write_chunk(FILE* fout, xspng_chunkp chunk);
+	
 #ifdef __cplusplus
-	} //extern "C"
-} //namespace xspng
-
+} //extern "C"
 #endif
 
 
