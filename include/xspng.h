d9bed9df081ce5b9e7fe2c80da283b4f8f51355a
Duncan Burggraf <dcburggraf@gmail.com>
2016-06-24 13:31:16 -0700

    added license terms to the public-facing header files

--- include/xspng.h
+++ include/xspng.h
@@ -5,44 +5,43 @@
 #include <stdio.h>
 #include <stdint.h>
 
-/* *
+/* xspng.h 
  *
- * Using this library:
+ * Licensed: MIT
+ * authored: Duncan Burggraf
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Duncan Burggraf
  * 
- * Declare a xspng_imagep variable as so:
- *
- *		#	xspng_int width = 256, height = 256; //four byte integer type
- *		#	xspng_imagep imgp = xspng_new_image(width,height);
- *
- * Iterate through x and y values ((0,0) is the upper-left-most pixel), setting
- * image sample values to the desired values.
- *
- *		#	xspng_byte r, g, b; //1 byte unsigned integer type
- *		#	xspng_int x,y;
- *		#	for (y = 0; y < height; y++) {
- *		#		for (x = 0; x < width; x++) {
- *		#			r = 0xff & x;
- *		#			g = 0xff & y;
- *		#			b = 0xff & ((x + y) / 2);
- *		#			xspng_set_rgb(imgp,x,y,r,g,b);
- *		#		}
- *		#	}
- *
- *	Write the image to file
- *
- *		#	xspng_write_to_file(imgp, "output.png");
- *
- * */
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 
 
-
-/* don't mangle our declarations with a C++ compiler */
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
 
+
+	/* * * * * Typedefs * * * * */
 
 	typedef uint8_t  xspng_byte;
 	typedef uint32_t xspng_int;
@@ -56,10 +55,22 @@ extern "C" {
 	//pointer to the above-defined struct
 	typedef xspng_image* xspng_imagep;
 
-	//struct initialization functions
+
+
+	/* * * * * struct initialization functions * * * * */
+
+	//return a pointer to a new image struct with allocated buffer
 	xspng_imagep xspng_image_new(xspng_int width, xspng_int height);
+
+	//free an image struct and it's buffer
 	void xspng_image_free(xspng_imagep imgp);
+
+	//free old buffer (if any) and allocate a new one
 	void xspng_image_init(xspng_image *img);
+
+
+
+	/* * * * * Pixel Setting Functions * * * * */
 
 	//set a pixel to the given red-green-blue samples
 	void xspng_image_set_rgb(
@@ -75,15 +86,17 @@ extern "C" {
 			xspng_byte r, xspng_byte g, xspng_byte b, xspng_byte a
 		);
 
-	//write a xspng_image struct to file
+
+
+	/* * * * * Image Writing Functions * * * * */
+
 	void xspng_image_write(xspng_imagep, const char * filename);
 
 
 
-/* end anti-prototype-mangling protections */
 #ifdef __cplusplus
-} // extern "C"
-#endif /* __cplusplus */
+} 
+#endif /* __cplusplus */ 
 
 
 
--- include/xspngpp.h
+++ include/xspngpp.h
@@ -2,6 +2,34 @@
 #ifndef __XSPNGPP_H
 #define __XSPNGPP_H
 
+/* xspngpp.h 
+ *
+ * Licensed: MIT
+ * authored: Duncan Burggraf
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Duncan Burggraf
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <xspng.h>
 
 namespace xspng {
@@ -13,8 +41,7 @@ namespace xspng {
 			Color();
 			Color(unsigned char r, unsigned char g, unsigned char b);
 			Color(unsigned int r, unsigned int g, unsigned int b);
-			Color(unsigned int r, 
-					unsigned int g, unsigned int b, unsigned int a);
+			Color(unsigned int r, unsigned int g, unsigned int b, unsigned int a);
 			Color(double r, double g, double b);
 			Color(double r, double g, double b, double a);
 	};
@@ -40,3 +67,4 @@ namespace xspng {
 }
 
 #endif
+
